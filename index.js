a3dafe2af07453842e0a22de16205e337240c02d
yourdilemma <k.iankovskaia@gmail.com>
2021-01-17 17:12:08 -0500

    created an array of questions

--- index.js
+++ index.js
@@ -5,3 +5,65 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+// TODO: Create an array of questions for user input
+
+const questions = [
+    {
+    type: "input",
+    question: "Please, enter your project title:",
+    name: "Title"
+    },
+    {
+    type: "input",
+    question: "Describe your application",
+    name: "Description"
+    },
+    {
+    type: "input",
+    question: "Provide installation process info",
+    name: "Installation"
+    },
+    {
+    type: "input",
+    question: "It's helpful to have inline the smallest example of usage that you can demonstrate.",
+    name: "Usage"
+    },
+    {
+    type: "input",
+    question: "Who was involved in the making of this code?",
+    name: "Credits"
+    },
+    {
+    type: "list",
+    question: "What license do you have",
+    name: "License",
+    choices: [
+        "MIT",
+        "GPLv3",
+        "Apache License 2.0",
+        "BSD" 
+    ]
+    },
+    {
+    type: "input",
+    question: "Enter the contribution guidelines",
+    name: "Contributing"
+    },
+    {
+    type: "input",
+    question: "Include tests here",
+    name: "Tests"
+    },
+    {
+    type: "input",
+    question: "What is your Email Address?",
+    name: "Contact"
+    },
+    {
+    type: "input",
+    question: "Enter contact information for additional questions",
+    name: "Questions"
+    },
+];
+
+
