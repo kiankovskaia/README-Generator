cdb94cd4f5e4a02ae64db08a12b21d78aa127555
yourdilemma <k.iankovskaia@gmail.com>
2021-01-17 17:07:22 -0500

    added dependencies

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
+
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
